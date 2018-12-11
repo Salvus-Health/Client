5e4fa9757560747bb9314e73d804e8bd8653c6b2
jordiahl <jordiahl@ufl.edu>
2018-12-11 12:11:55 -0500

    adding routes

---
+++ src/app/routes/DeployRoutes.ts
@@ -0,0 +1,4 @@
+export const enum DeployRoutes {
+    heroku = "https://salvus-server.herokuapp.com/"
+}
+    
--- src/app/routes/ServerRoutes.ts
+++ src/app/routes/ServerRoutes.ts
@@ -1,3 +1,5 @@
+import { DeployRoutes } from "./DeployRoutes";
+
 /*--------------------------------------------------------------------------------------
 |	ServerRoutes Module: Created by Jordi Hernandez on 10/26/2018.
 |---------------------------------------------------------------------------------------
@@ -6,9 +8,10 @@
 ---------------------------------------------------------------------------------------*/
 
 export const enum ServerRoutes {
-
-  register = 'http://localhost:9000/users/',
-  login = 'http://localhost:9000/authenticate',
-  getDoctors = 'http://localhost:9000/doctors',
-
+  // register = 'http://localhost:9000/users/',
+  register = 'https://salvus-server.herokuapp.com/users/',
+  // login = 'http://localhost:9000/authenticate',
+  login = 'https://salvus-server.herokuapp.com/authenticate',
+  // getDoctors = 'http://localhost:9000/doctors',
+  getDoctors = 'https://salvus-server.herokuapp.com/doctors',
 }
